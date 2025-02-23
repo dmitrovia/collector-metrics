9679c23b6c7fb7b9cec90bdbfbcbe4f0c02e49c1
WeIsWeAre <robokop35@mail.ru>
2025-02-23 22:38:17 +0300

    fix: urls

--- internal/handlers/sender/sender_test.go
+++ internal/handlers/sender/sender_test.go
@@ -161,7 +161,7 @@ func setHandlerParams(params *bizmodels.InitParams) error {
 	params.
 		ValidateAddrPattern = "^[a-zA-Z/ ]{1,100}:[0-9]{1,10}$"
 	params.DatabaseDSN = "postgres://postgres:postgres" +
-		"@localhost" +
+		"@postgres" +
 		":5432/praktikum?sslmode=disable"
 	_, path, _, ok := runtime.Caller(0)
 
